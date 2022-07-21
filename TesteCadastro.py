1de757fb1c5fcb9e7395c0b00ef7ab624a48d7d8
Juan Costa <juancostask@gmail.com>
2022-07-21 08:30:56 -0300

    teste2

---
+++ TesteCadastro.py
@@ -0,0 +1,11 @@
+from ast import keyword
+
+
+user = ''
+senha = ''
+
+while user !='juan':
+    while senha !='123456':
+        user=input('Digite seu usuario:\n')
+        senha=input('Digite sua senha:\n')
+print('Bem-vindo Juan!')
